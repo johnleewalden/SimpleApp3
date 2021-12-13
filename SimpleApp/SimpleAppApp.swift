abf07f8d7b3f6f87ef5c3bcfd2bce3608a566d0e
johnleewalden <johnleewalden@gmail.com>
2021-12-13 13:42:33 -0500

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by John Walden on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
